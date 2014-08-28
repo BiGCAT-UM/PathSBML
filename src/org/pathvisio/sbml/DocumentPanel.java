7f6ff4d6085b7eb1779c3b6720f90237f3c949e0
Sri Harsha <kmitharsha@gmail.com>
2014-08-28 22:13:39 +0530

    Make the SBML pane editable #7- enhancement completed

--- src/org/pathvisio/sbml/DocumentPanel.java
+++ src/org/pathvisio/sbml/DocumentPanel.java
@@ -18,6 +18,9 @@
 package org.pathvisio.sbml;
 
 import java.awt.BorderLayout;
+import java.util.EventObject;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -25,11 +28,24 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.SwingUtilities;
+import javax.swing.event.TreeModelEvent;
+import javax.swing.event.TreeModelListener;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellEditor;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreeSelectionModel;
 
 import org.pathvisio.core.ApplicationEvent;
 import org.pathvisio.core.Engine;
 import org.pathvisio.core.Engine.ApplicationEventListener;
+import org.pathvisio.core.model.ObjectType;
+import org.pathvisio.core.model.Pathway;
+import org.pathvisio.core.model.PathwayElement;
+import org.sbml.jsbml.Species;
 
 /**
  * This class adds action to the SBML side pane.
@@ -42,12 +58,14 @@ import org.pathvisio.core.Engine.ApplicationEventListener;
  * @version 1.0.0
  * 
  */
-public class DocumentPanel extends JPanel implements ApplicationEventListener
-{
+public class DocumentPanel extends JPanel implements ApplicationEventListener {
 	// private SBMLDocument lastImported = null;
 	// Desktop desktop;
 	JScrollPane treePane = new JScrollPane();
 	private final ExecutorService executor;
+	private Engine engine;
+	private String selectednode;
+	Pathway pathway;
 
 	// SBMLDocument document;
 
@@ -56,7 +74,9 @@ public class DocumentPanel extends JPanel implements ApplicationEventListener
 		treePane = new JScrollPane(new JTree(SBMLFormat.modelDoc));
 		add(treePane);
 		engine.addApplicationEventListener(this);
+		this.engine = engine;
 		executor = Executors.newSingleThreadExecutor();
+
 	}
 
 	/**
@@ -66,7 +86,7 @@ public class DocumentPanel extends JPanel implements ApplicationEventListener
 	 */
 	@Override
 	public void applicationEvent(ApplicationEvent e) {
-		switch(e.getType()) {
+		switch (e.getType()) {
 		case PATHWAY_NEW:
 			clean();
 			break;
@@ -104,9 +124,33 @@ public class DocumentPanel extends JPanel implements ApplicationEventListener
 					@Override
 					public void run() {
 						remove(treePane);
-						JTree elementTree = new JTree();
-						TreeModel elementModel = new NavigationTree(
-								SBMLFormat.modelDoc).getTreeModel();
+						final JTree elementTree = new JTree();
+						elementTree.setEditable(true);
+						
+				        MyTreeCellEditor editor = new MyTreeCellEditor(elementTree,(DefaultTreeCellRenderer) elementTree.getCellRenderer());
+				        elementTree.setCellEditor(editor);
+						elementTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+						elementTree.addTreeSelectionListener(new TreeSelectionListener() {
+									public void valueChanged(TreeSelectionEvent e) {
+										DefaultMutableTreeNode node = (DefaultMutableTreeNode) elementTree.getLastSelectedPathComponent();
+										if (node == null)
+											return;
+										Object nodeInfo = node.getUserObject();
+										if (node.isLeaf()) {
+											if (nodeInfo instanceof Species) {
+												Species sp = (Species) nodeInfo;
+												selectednode = sp.getId();
+
+											} else {
+												selectednode = nodeInfo.toString();
+											}
+
+										}
+									}
+								});
+
+						TreeModel elementModel = new NavigationTree(SBMLFormat.modelDoc).getTreeModel();
+						elementModel.addTreeModelListener(new MyTreeModelListener());
 						elementTree.setModel(elementModel);
 						treePane = new JScrollPane(elementTree);
 						add(treePane);
@@ -136,5 +180,80 @@ public class DocumentPanel extends JPanel implements ApplicationEventListener
 		});
 	}
 
+	private static class MyTreeCellEditor extends DefaultTreeCellEditor {
 
+        public MyTreeCellEditor(JTree tree, DefaultTreeCellRenderer renderer) {
+            super(tree, renderer);
+        }
+
+       
+
+        @Override
+        public boolean isCellEditable(EventObject e) {
+            return super.isCellEditable(e)
+                && ((TreeNode) lastPath.getLastPathComponent()).isLeaf();
+        }
+    }
+	class MyTreeModelListener implements TreeModelListener {
+		public void treeNodesChanged(TreeModelEvent e) {
+			DefaultMutableTreeNode node;
+			node = (DefaultMutableTreeNode) (e.getTreePath().getLastPathComponent());
+
+			try {
+				int index = e.getChildIndices()[0];
+				node = (DefaultMutableTreeNode) (node.getChildAt(index));
+			} catch (NullPointerException exc) {
+			}
+
+			// update the related objects
+			pathway = engine.getActivePathway();
+			PathwayElement pe = pathway.getElementById(selectednode);
+
+			List<PathwayElement> pelemets = pathway.getDataObjects();
+			
+			for (Iterator iterator = pelemets.iterator(); iterator.hasNext();) {
+				PathwayElement pathwayElement = (PathwayElement) iterator
+						.next();
+				if (pathwayElement.getObjectType() == ObjectType.LINE) {
+					if (pathwayElement.getStartGraphRef().equalsIgnoreCase(selectednode))
+						pathwayElement.setStartGraphRef((String) node.getUserObject());
+					if (pathwayElement.getEndGraphRef().equalsIgnoreCase(selectednode))
+						pathwayElement.setEndGraphRef((String) node.getUserObject());
+
+				}
+
+			}
+
+			if (pe.getObjectType() == ObjectType.DATANODE) {
+				pe.setTextLabel((String) node.getUserObject());
+				pe.setGraphId((String) node.getUserObject());
+			}
+			if (pe.getObjectType() == ObjectType.LABEL) {
+				if (pe.getGraphId().equalsIgnoreCase(selectednode))
+					pe.setGraphId((String) node.getUserObject());
+
+			}
+
+		}
+
+		@Override
+		public void treeNodesInserted(TreeModelEvent e) {
+			
+			
+		}
+
+		@Override
+		public void treeNodesRemoved(TreeModelEvent e) {
+			
+			
+		}
+
+		@Override
+		public void treeStructureChanged(TreeModelEvent e) {
+		
+			
+		}
+
+		
+	}
 }
